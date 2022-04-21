4d7f0e6e0d3eb2d8f7f4458426d715515fda0d64
Moises <moisescampos1234@gmail.com>
2022-04-21 01:06:53 -0430

    se agrego un temporizar al programa

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,6 +1,16 @@
 import os
+import time
+
+segundos = 10
 
 print('BIENVENIDO')
+for i in range(0,segundos):
+        print(' ')
+        print(f'Segundos: {segundos}')
+        time.sleep(1)
+        os.system('cls')
+        segundos -= 1
 print('olo')
 
 os.system('notepad.exe')
+
