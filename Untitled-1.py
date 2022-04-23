0d5dd28eef839b670c2e02d47e6ec4c120b49f0e
Moises DELL <moisescampos1234@gmail.com>
2022-04-23 01:50:30 -0430

    Se cambio la estructura del programa. La idea es empezar a crear un juego

--- Untitled-1.py
+++ Untitled-1.py
@@ -4,15 +4,20 @@ import time
 segundos = 10
 
 print('BIENVENIDO')
-for i in range(0,segundos):
-        print(' ')
-        print(f'Segundos: {segundos}')
-        time.sleep(1)
-        os.system('cls')
-        segundos -= 1
-print('Prueba escribir un texto en el blog de notas')
 
-os.system('notepad.exe')
+desicion = input('''
+Quieres jugar un juego?
+S para si y N para no --> ''').lower()
 
+if desicion == 's':
+        print('Comenzamos en...')
+        for i in range(0,segundos):
+                print(' ')
+                print(f'Segundos: {segundos}')
+                time.sleep(1)
+                os.system('cls')
+                segundos -= 1
+else:
+        print('Aburrid@ :(')
 
 
