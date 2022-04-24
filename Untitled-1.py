8facfa2c1f813e3652e135de159bc9482c503542
Moises DELL <moisescampos1234@gmail.com>
2022-04-23 23:54:09 -0430

    Se mocidifico la cantidad de segundos y el texto de bienvenida al juego

--- Untitled-1.py
+++ Untitled-1.py
@@ -5,8 +5,9 @@ import random
 def adivinanza():
         
     vidas = 6
-    print(vidas)
+    print(f'Tienes {vidas} vidas')
     print(' ')
+    print('Tienes que adivinar el numero que escogio la computadora')
     numero_escogido = int(input('Ingresa un numero del 1 al 100: '))
     numero_aleatorio = random.randint(1, 100)
   
@@ -34,25 +35,25 @@ def adivinanza():
         
         
 def run():
-    segundos = 10
+    segundos = 5
 
-        print('BIENVENIDO')
+    print('BIENVENIDO')
 
-        desicion = input('''
-        Quieres jugar un juego?
-        S para si y N para no --> ''').lower()
+    desicion = input('''
+    Quieres jugar un juego?
+    S para si y N para no --> ''').lower()
 
-        if desicion == 's':
-                print('Comenzamos en...')
-                for i in range(0,segundos):
-                        print(' ')
-                        print(f'Segundos: {segundos}')
-                        time.sleep(1)
-                        os.system('cls')
-                        segundos -= 1
-                adivinanza()
-        else:
-                print('Aburrid@ :(')
+    if desicion == 's':
+        print('Comenzamos en...')
+        for i in range(0,segundos):
+            print(' ')
+            print(f'Segundos: {segundos}')
+            time.sleep(1)
+            os.system('cls')
+            segundos -= 1
+        adivinanza()
+    else:
+        print('Aburrid@ :(')
     
 
 if __name__ == '__main__':
