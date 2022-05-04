f06cc92f0bc9483e3474f4eb09ffc5953a4cb6f7
Moises DELL <moisescampos1234@gmail.com>
2022-05-04 06:20:40 -0430

    se agrego el juego del ahorcado y se aplico una division de edades para entrar a los diversos juegos

--- Untitled-1.py
+++ Untitled-1.py
@@ -2,14 +2,59 @@ import os
 import time
 import random
 
+def ahorcado():
+
+    meses = ['enero','febrero','marzo','abril','mayo','junio','julio',
+    'agosto','septiembre','octubre','noviembre','diciembre']
+
+    palabra_aleatoria = random.choice(meses)
+    
+    espacios = ['_'] * len(palabra_aleatoria)
+    lista_letras = ''
+
+    vidas = 5 
+    print(f'Tienes {vidas} vidas en total')
+
+    while True:
+
+        for i in espacios:
+            print(i,end=' ')
+
+        print(' ')
+        print(' ')
+
+        letra_usuario = input('Ingresa una letra: ')
+        
+        for numero, valor in enumerate(palabra_aleatoria):
+            if valor == letra_usuario:
+                espacios[numero] = letra_usuario
+
+        if letra_usuario not in palabra_aleatoria:
+            if vidas == 1:
+                print('Vidas = 0, Suerte para la proxima!')
+                break
+            else:
+                print(' ')
+                print('---> No se encuentra <---')
+                vidas -= 1
+                print(f'Te quedan {vidas} vidas')
+                print(' ')
+        
+        if '_' not in espacios:
+            print('GANASTE!')
+            break
+
+
 def adivinanza():
         
     vidas = 7
+
+    print('Adivina el numero que escogio la computadora...')
     print(f'Tienes {vidas} vidas')
     print(' ')
-    print('Tienes que adivinar el numero que escogio la computadora')
     numero_escogido = int(input('Ingresa un numero del 1 al 50: '))
     numero_aleatorio = random.randint(1, 50)
+    print(' ')
   
     while numero_escogido != numero_aleatorio and vidas >= 2:
 
@@ -33,37 +78,45 @@ def adivinanza():
         print('Suerte para la proxima')
         print(' ')
         
-        
-def run(nombre):
+
+def datos():
     segundos = 5
 
-    print("La tierra es el tercer planeta del distema solar")
+    print(' ')
+    nombre = input('Cual es tu nombre? -> ')
+    edad = int(input(f'Que edad tienes {nombre}? -> '))
+    nombre = nombre.upper()
+    print(' ')
 
-    desicion = input('''
-    Quieres jugar un juego?
-    S para si y N para no --> ''').lower()
-
-    if desicion == 's':
-        print('Comenzamos en...')
-        for i in range(0,segundos):
+    print('Comenzamos en...')
+    for i in range(0,segundos):
             print(' ')
             print(f'Segundos: {segundos}')
             time.sleep(1)
             os.system('cls')
             segundos -= 1
+
+    if edad <= 12:
+        print(f'BIENVENIDO {nombre}')
         adivinanza()
     else:
+        print(f'BIENVENIDO AL JUEGO DEL AHORCADO {nombre}')
+        ahorcado()
+
+
+def run():
+    
+    desicion = input('''
+    Quieres jugar un juego?
+    S para si y N para no --> ''').lower()
+    
+    if desicion == 's':
+        datos()
+    else:
         print('Aburrid@ :(')
-    
 
-def datos():
-    print(' ')
-    nombre = input('Cual es tu nombre? -> ').upper()
-    edad = int(input('Que edad tienes? -> '))
-    print('----------------------------------------------')
-    run(nombre)
+  
+if __name__ == '__main__':
+    run()
 
-    print('Master 1')
-    datos()
 
-    
